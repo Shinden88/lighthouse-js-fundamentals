378cd3a466ea1b65fcf43f241adf4aae191609e1
Matija <matija_bozic@yahoo.com>
2021-10-13 12:20:46 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -13,3 +13,32 @@ console.log("short sleeves are fine.");
 }
 
 console.log("Now you're ready to go outside!");
+
+/* new code */
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+console.log("You are eligible to vote.")
+}
+
+
+/* new code */
+
+const temp = 41;
+
+if (temp < -40 || temp > 40) {
+console.log("Maybe going outside isn't such a great idea...");
+}
+
+/* new code */
+
+const raining = false;
+
+if (!raining) {
+console.log("Leave your umbrella at home.");
+}
+
+
+
